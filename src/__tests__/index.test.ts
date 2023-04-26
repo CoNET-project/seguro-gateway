3c42e3e0677552b56882262f16e16a5b086ca093
CoNET <info@CoNETTech.ca>
2023-04-25 21:31:46 -0700

    fix build bug

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,5 @@
-import { Person, getGreeting } from '../utilities/utilities'
+
 
 describe('public interface', () => {
 
-    it('should have getGreeting function', () => {
-        expect(typeof getGreeting).toEqual('function')
-    })
-
-    it('should have Person type', () => {
-        const person: Person = { name: 'John' }
-        expect(typeof person).toEqual('object')
-    })
 })
